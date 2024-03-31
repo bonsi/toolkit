52a8284b5c927ff178997348eef318ef0c656205
Ivo <bonsi@protonmail.com>
2024-03-31 23:50:20 +0200

    9. Trying out our new module in a simple Go application

--- tools.go
+++ tools.go
@@ -4,8 +4,12 @@ import "crypto/rand"
 
 const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
 
+// Tools is the type used to instantiate this module.
+// Any variable of this type will have access to all the methods with the receiver *Tools
 type Tools struct{}
 
+// RandomString returns a string of random characters of length n, using randomStringSource
+// as the source for the string
 func (t *Tools) RandomString(n int) string {
 	s, r := make([]rune, n), []rune(randomStringSource)
 	for i := range s {
